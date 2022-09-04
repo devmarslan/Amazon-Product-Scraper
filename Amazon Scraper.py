8248d5a236e91ff231d51b56f0204aa9bb0d584d
Muhammad Arslan <49634814+MuhammadArslanArain@users.noreply.github.com>
2022-09-04 19:28:58 +0500

    Removed Useless Lines

--- Amazon Scraper.py
+++ Amazon Scraper.py
@@ -1,20 +1,14 @@
 import csv
 import time
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from urllib import request,response
+from urllib import request, response
 from selenium.webdriver.common.by import By
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 link = input("Enter Link: ")
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# a1 = driver.find_element(By.XPATH, '//*[@id="a-autoid-7-announce"]/img').get_attribute("src")
-# print(a1)
-
 
 record_no = 0
 page_no = 1
@@ -28,10 +22,12 @@ while True:
     for i in results:
         print(counter)
         try:
-            driver.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div['+str(counter)+']/div/div/div/div/div[2]/div[1]/h2/a').click()
+            driver.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div['+str(
+                counter)+']/div/div/div/div/div[2]/div[1]/h2/a').click()
             url = driver.current_url
             try:
-                title = driver.find_element(By.XPATH, '//*[@id="productTitle"]').text
+                title = driver.find_element(
+                    By.XPATH, '//*[@id="productTitle"]').text
             except:
                 title = ""
             try:
@@ -39,19 +35,23 @@ while True:
             except:
                 price = ""
             try:
-                delivery = driver.find_element(By.XPATH, '//*[@id="mir-layout-DELIVERY_BLOCK-slot-PRIMARY_DELIVERY_MESSAGE_LARGE"]/span').text
+                delivery = driver.find_element(
+                    By.XPATH, '//*[@id="mir-layout-DELIVERY_BLOCK-slot-PRIMARY_DELIVERY_MESSAGE_LARGE"]/span').text
             except:
                 delivery = ""
             try:
-                stock = driver.find_element(By.XPATH, '//*[@id="availability"]/span').text
+                stock = driver.find_element(
+                    By.XPATH, '//*[@id="availability"]/span').text
             except:
                 stock = ""
             try:
-                color = driver.find_element(By.XPATH, '//*[@id="variation_color_name"]/div/span').text
+                color = driver.find_element(
+                    By.XPATH, '//*[@id="variation_color_name"]/div/span').text
             except:
                 color = ""
             try:
-                sold_by = driver.find_element(By.XPATH, '//*[@id="tabular-buybox"]/div[1]/div[4]').text
+                sold_by = driver.find_element(
+                    By.XPATH, '//*[@id="tabular-buybox"]/div[1]/div[4]').text
             except:
                 sold_by = ""
             try:
@@ -59,31 +59,38 @@ while True:
             except:
                 brand = ""
             try:
-                model_name = driver.find_element(By.CLASS_NAME, "po-model_name").text
+                model_name = driver.find_element(
+                    By.CLASS_NAME, "po-model_name").text
             except:
                 model_name = ""
             try:
-                age_range = driver.find_element(By.CLASS_NAME, "po-age_range_description").text
+                age_range = driver.find_element(
+                    By.CLASS_NAME, "po-age_range_description").text
             except:
                 age_range = ""
             try:
-                power_source = driver.find_element(By.CLASS_NAME, "po-power_source_type").text
+                power_source = driver.find_element(
+                    By.CLASS_NAME, "po-power_source_type").text
             except:
                 power_source = ""
             try:
-                about_item = driver.find_element(By.XPATH, '//*[@id="feature-bullets"]/ul').text
+                about_item = driver.find_element(
+                    By.XPATH, '//*[@id="feature-bullets"]/ul').text
             except:
                 about_item = ""
             try:
-                product_dimension = driver.find_element(By.XPATH, '//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[1]/td').text
+                product_dimension = driver.find_element(
+                    By.XPATH, '//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[1]/td').text
             except:
                 product_dimension = ""
             try:
-                item_weight = driver.find_element(By.XPATH, '//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[2]/td').text
+                item_weight = driver.find_element(
+                    By.XPATH, '//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[2]/td').text
             except:
                 item_weight = ""
             try:
-                safety_info = driver.find_element(By.XPATH, '//*[@id="important-information"]/div/p[2]').text
+                safety_info = driver.find_element(
+                    By.XPATH, '//*[@id="important-information"]/div/p[2]').text
             except:
                 safety_info = ""
             img1 = "1"
@@ -94,23 +101,28 @@ while True:
             for x in range(1, 20):
                 if img1 == "1" or '360' in img1:
                     try:
-                        img1 = driver.find_element(By.XPATH, '//*[@id="a-autoid-' + str(x) + '-announce"]/img').get_attribute("src")
+                        img1 = driver.find_element(
+                            By.XPATH, '//*[@id="a-autoid-' + str(x) + '-announce"]/img').get_attribute("src")
                     except:
                         img1 = "1"
                     try:
-                        img2 = driver.find_element(By.XPATH, '//*[@id="a-autoid-' + str(x + 1) + '-announce"]/img').get_attribute("src")
+                        img2 = driver.find_element(
+                            By.XPATH, '//*[@id="a-autoid-' + str(x + 1) + '-announce"]/img').get_attribute("src")
                     except:
                         img2 = "2"
                     try:
-                        img3 = driver.find_element(By.XPATH, '//*[@id="a-autoid-' + str(x + 2) + '-announce"]/img').get_attribute("src")
+                        img3 = driver.find_element(
+                            By.XPATH, '//*[@id="a-autoid-' + str(x + 2) + '-announce"]/img').get_attribute("src")
                     except:
                         img3 = "3"
                     try:
-                        img4 = driver.find_element(By.XPATH, '//*[@id="a-autoid-' + str(x + 3) + '-announce"]/img').get_attribute("src")
+                        img4 = driver.find_element(
+                            By.XPATH, '//*[@id="a-autoid-' + str(x + 3) + '-announce"]/img').get_attribute("src")
                     except:
                         img4 = "4"
                     try:
-                        img5 = driver.find_element(By.XPATH, '//*[@id="a-autoid-' + str(x + 4) + '-announce"]/img').get_attribute("src")
+                        img5 = driver.find_element(
+                            By.XPATH, '//*[@id="a-autoid-' + str(x + 4) + '-announce"]/img').get_attribute("src")
                     except:
                         img5 = "5"
                 else:
@@ -123,7 +135,8 @@ while True:
                     color = ""
 
             edit_price = about_item.replace("【", "[")
-            record = [url, title, str(price), delivery, stock, color, sold_by, brand[6:], model_name[11:], age_range[24:], power_source[13:], edit_price.replace("】", "]"), product_dimension, item_weight, safety_info, img1.replace("US40_", ""), img2.replace("US40_", ""), img3.replace("US40_", ""), img4.replace("US40_", ""), img5.replace("US40_", "")]
+            record = [url, title, str(price), delivery, stock, color, sold_by, brand[6:], model_name[11:], age_range[24:], power_source[13:], edit_price.replace(
+                "】", "]"), product_dimension, item_weight, safety_info, img1.replace("US40_", ""), img2.replace("US40_", ""), img3.replace("US40_", ""), img4.replace("US40_", ""), img5.replace("US40_", "")]
             records.append(record)
             record_no = record_no + 1
             print(record)
@@ -141,5 +154,6 @@ while True:
 
 with open('results.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(['Url', 'Title', 'Price', 'Delivery', 'Stock', 'Colour', 'Sold By', 'Brand', 'Model Name', 'Age Range', 'Power Source', 'About Item', 'Product Dimension', 'Item Weight', 'Safety Info', 'Img1', 'Img2', 'Img3', 'Img4', 'Img5'])
+    writer.writerow(['Url', 'Title', 'Price', 'Delivery', 'Stock', 'Colour', 'Sold By', 'Brand', 'Model Name', 'Age Range',
+                    'Power Source', 'About Item', 'Product Dimension', 'Item Weight', 'Safety Info', 'Img1', 'Img2', 'Img3', 'Img4', 'Img5'])
     writer.writerows(records)
